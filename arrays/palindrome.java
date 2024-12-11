aae980aca7757639a6aa452a75aba249e57c7aea
Aarushi <128125181+aarushigangta@users.noreply.github.com>
2024-12-11 18:58:51 +0530

    Add files via upload

---
+++ arrays/palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String num=x+"";
+        int left=0;
+        int right=num.length()-1;
+
+        while(left<right){
+            if(num.charAt(left)!=num.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
